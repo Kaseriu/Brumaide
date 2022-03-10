d5b307c18fade9368a1b29d8827bc6f8c4bdbd4e
kevin <kevin.mazure.km@gmail.com>
2022-03-10 11:17:50 +0100

    avancement

--- src/App.js
+++ src/App.js
@@ -1,75 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
-import {useState} from "react";
-const socket = io('localhost:8000');
+import Message from './Message';
+import MessageInput from './MessageInput';
 
+import './App.css';
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  return (
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <div className="card">
-              <div className="card-body">
-                <div className="card-title">My first chat</div>
-                <hr/>
-                <div className="messages">
-                  {messages.map(msg => {
-                    return (
-                        <div key={true}>{msg.username}: {msg.text}</div>
-                    )
-                  })}
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        const newSocket = io(`http://${window.location.hostname}:3000`);
+        setSocket(newSocket);
+        return () => newSocket.close();
+    }, [setSocket]);
+
+    return (
+        <div className="App">
+            <header className="app-header">
+                React Chat
+            </header>
+            { socket ? (
+                <div className="chat-container">
+                    <Message socket={socket} />
+                    <MessageInput socket={socket} />
                 </div>
-              </div>
-              <form onSubmit={e => sendMessage(e)}>
-                <div className="card-footer">
-                  <input id="username"
-                         type="text"
-                         placeholder="Username"
-                         className="form-control"
-                  />
-                  <br/>
-                  <input id="text"
-                         type="text"
-                         placeholder="Your message"
-                         className="form-control"
-                  />
-                  <br/>
-                  <button type="submit"
-                          className="btn btn-primary form-control">
-                    send
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
+            ) : (
+                <div>Not Connected</div>
+            )}
         </div>
-      </div>
-  );
-
-  function setNewMessage(msg) {
-    setMessages([
-      ...messages,
-      msg
-    ]);
-  }
-
-  function sendMessage(e) {
-    e.preventDefault();
-    const msg = {
-      username: e.target.username.value,
-      text: e.target.text.value
-    };
-    socket.emit('CLIENT_MSG', msg);
-    setNewMessage(msg);
-  }
-
-
-  socket.on('SERVER_MSG', msg => {
-    setNewMessage(msg);
-  });
+    );
 }
 
 export default App;
