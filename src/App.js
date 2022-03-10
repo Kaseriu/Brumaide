f4a02600ed9a2d308af4d9e1637ddff68b8a1506
kevin <kevin.mazure.km@gmail.com>
2022-03-10 09:43:47 +0100

    Début du chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import {useState} from "react";
+const socket = io('localhost:8000');
+
 
 function App() {
+  const [messages, setMessages] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">My first chat</div>
+                <hr/>
+                <div className="messages">
+                  {messages.map(msg => {
+                    return (
+                        <div key={true}>{msg.username}: {msg.text}</div>
+                    )
+                  })}
+                </div>
+              </div>
+              <form onSubmit={e => sendMessage(e)}>
+                <div className="card-footer">
+                  <input id="username"
+                         type="text"
+                         placeholder="Username"
+                         className="form-control"
+                  />
+                  <br/>
+                  <input id="text"
+                         type="text"
+                         placeholder="Your message"
+                         className="form-control"
+                  />
+                  <br/>
+                  <button type="submit"
+                          className="btn btn-primary form-control">
+                    send
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
   );
+
+  function setNewMessage(msg) {
+    setMessages([
+      ...messages,
+      msg
+    ]);
+  }
+
+  function sendMessage(e) {
+    e.preventDefault();
+    const msg = {
+      username: e.target.username.value,
+      text: e.target.text.value
+    };
+    socket.emit('CLIENT_MSG', msg);
+    setNewMessage(msg);
+  }
+
+
+  socket.on('SERVER_MSG', msg => {
+    setNewMessage(msg);
+  });
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
