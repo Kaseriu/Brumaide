9262a6f86e2396ff025858a87b4b216295557d53
Luigi <luigi.carole@gmail.com>
2022-03-11 14:17:00 +0100

    Mise à jour de la page Issue

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Issue from './containers/issue/Issue';
-import Chat from './containers/Chat';
-import Dashboard from './containers/Dashboard';
 import Home from './containers/home/Home';
-import Issue from './containers/Issue';
 import Chat from './containers/chat/Chat';
 import Dashboard from './containers/dashboard/Dashboard';
 import Register from './containers/register/Register';
+
 import {
   BrowserRouter as Router,
   Switch,
--- app/src/containers/issue/Issue.js
+++ app/src/containers/issue/Issue.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import {withRouter} from 'react-router';
 import './Issue.css';
 
 function Issue() {
+    const [data, setData] = useState([]);
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+    const [Tags, setTags] = useState(1);
+    const [modify, setModify] = useState(1);
+
+    useEffect(() =>{
+        if(modify !== 1){
+            const issue = {title: title, description: description, tags: Tags,ownerId: 1,priority:3,statusId:4}
+            console.log(issue);
+            fetch('http://10.33.5.57:3001/api/v1/issues',{
+                method:'POST',
+                body: JSON.stringify(issue),
+                headers: {'Content-Type': 'application/json'}
+            }).then(async(res)=>{
+                const json = await res.json();
+                setData(json)
+            }).catch(err=>{
+                console.log(err.message);
+            })
+            setModify(1);
+        }
+    });
+
     return(
         <div class="issue">
             <div class="center">
-                <form method="post">
+                <form>
                     <div class="txt_field">
-                        <input type="text" required/>
+                        <input id="Title" onChange={e => setTitle(e.target.value)} type="text" required/>
                         <span></span>
                         <label>Description</label>
                     </div>
                     <div class="issue-text">
-                        <textarea id="issue-text" name="issue-text"> Explain with details your issue</textarea>
+                        <textarea id="issue-text" onChange={e => setDescription(e.target.value)} placeholder="Explain with details your issue" name="issue-text"></textarea>
                     </div>
                     <div class="txt_field">
-                        <input type="text" required/>
+                        <input id="Tags" onChange={e => setTags(e.target.value)}  type="text" required/>
                         <span></span>
                         <label>Tags</label>
                     </div>
                     
-                    <input type="submit" value="Submit" onClick={() => console.log("click")}/>
+                    <input type="submit" value="Submit" onClick={() => setModify(0)}/>
                 </form>
                 </div>
 
@@ -43,4 +67,4 @@ function Issue() {
     );
 }
 
-export default withRouter(Issue);
+export default withRouter(Issue);   
